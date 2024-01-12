926533ebe00a2bc20be876cfe3f5e561e4c6a601
kgatt25036 <kgatt88@gmail.com>
2024-01-12 02:15:35 +0000

    update on database

---
+++ app/Models/Car.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+    protected $fillable = ['model', 'year', 'salesperson_email','manufacturer_id'];
+    public function manufacturers(){
+        return this->belongsTo(Manufacturer::class);
+    }
+}
+
